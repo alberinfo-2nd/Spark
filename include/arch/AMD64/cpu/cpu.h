d4ea0394da750f9b882e60eb0a847d6a4874472e
alberinfo-2nd <albertcuch@gmail.com>
2025-03-30 16:13:40 -0300

    allow inlining some fuctions

--- include/arch/AMD64/cpu/cpu.h
+++ include/arch/AMD64/cpu/cpu.h
@@ -3,10 +3,22 @@
 
 #include <types.h>
 
-extern void X86_CPU_cli(void);
-extern void X86_CPU_sti(void);
-extern void X86_CPU_hlt(void);
-extern void X86_CPU_cpuid(u32 function, u32 *eax, u32 *ebx, u32 *ecx, u32 *edx);
+inline void X86_CPU_cli(void) {
+    asm volatile("cli");
+}
+
+inline void X86_CPU_sti(void) {
+    asm volatile("sti");
+}
+
+inline void X86_CPU_hlt(void) {
+    asm volatile("hlt");
+}
+
+inline void X86_CPU_cpuid(u32 function, u32 *eax, u32 *ebx, u32 *ecx, u32 *edx) {
+    asm volatile("cpuid" : "=a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx) : "a" (function));
+}
+
 extern void X86_CPU_set_cr4_bit(u8 bit);
 
 #endif
--- src/arch/AMD64/cpu/cpu.c
+++ src/arch/AMD64/cpu/cpu.c
@@ -1,22 +1,6 @@
 #include <arch/AMD64/cpu/cpu.h>
 
-inline void X86_CPU_cli(void) {
-    asm volatile("cli");
-}
-
-inline void X86_CPU_sti(void) {
-    asm volatile("sti");
-}
-
-inline void X86_CPU_hlt(void) {
-    asm volatile("hlt");
-}
-
-inline void X86_CPU_cpuid(u32 function, u32 *eax, u32 *ebx, u32 *ecx, u32 *edx) {
-    asm volatile("cpuid" : "=a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx) : "a" (function));
-}
-
-inline void X86_CPU_set_cr4_bit(u8 bit) {
+void X86_CPU_set_cr4_bit(u8 bit) {
     u64 current_cr4 = 0;
     asm volatile("mov %%cr4, %0" : "=a" (current_cr4) : : );
     current_cr4 |= 1 << bit;
