5c87b10c264e00b55054a0b4d3875e4900f3300d
alberinfo-2nd <albertcuch@gmail.com>
2025-04-02 01:58:00 -0300

    Fix build error due to missing size operand

--- src/kernel/sync/spinlock.asm
+++ src/kernel/sync/spinlock.asm
@@ -3,17 +3,17 @@ section .text
 
 [GLOBAL SPINLOCK_acquire_lock]
 SPINLOCK_acquire_lock:
-    lock bts [rdi], 0 ; Atomically test and set bit 0
+    lock bts qword [rdi], 0 ; Atomically test and set bit 0
     jc .try_acquire
     ret
 
 .try_acquire:
     pause
-    test dword [rdi], 1
+    test qword [rdi], 1
     jnz .try_acquire
     jmp SPINLOCK_acquire_lock
 
 [GLOBAL SPINLOCK_release_lock]
 SPINLOCK_release_lock:
-    mov dword [rdi], 0
+    mov qword [rdi], 0
     ret
