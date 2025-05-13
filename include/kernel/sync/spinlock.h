7924cd34ab8f411b951df65d5f26d606403b2aff
alberinfo-2nd <albertcuch@gmail.com>
2025-05-13 01:03:47 -0300

    Add LOCK_FREE and LOCK_INUSE definitions

--- include/kernel/sync/spinlock.h
+++ include/kernel/sync/spinlock.h
@@ -1,6 +1,9 @@
 #ifndef SPINLOCK_H
 #define SPINLOCK_H
 
+#define LOCK_FREE   0
+#define LOCK_INUSE  1
+
 #include <types.h>
 
 //In the future, add code to handle task switching so that a task is put to sleep when it tries to lock on a resource
