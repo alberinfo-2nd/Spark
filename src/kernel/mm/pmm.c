a7e9c42048db7e2683d6f568288a454860364a72
alberinfo-2nd <albertcuch@gmail.com>
2025-06-10 21:46:23 -0300

    Fix various bugs, specially memory overwrite due to lack of consideration for KERNEL_END address, implement multi-page allocation

--- src/kernel/mm/pmm.c
+++ src/kernel/mm/pmm.c
@@ -9,6 +9,8 @@
 
 #define PMM_map_size 4096 //One single entry on the bitmap is 4KiB, same as the smallest MMU entry possible
 
+extern u8 KERNEL_END;
+
 struct PMM_memory_block_t {
     u64 *bitmap;
     u64 bitmap_size;
@@ -60,6 +62,14 @@ struct PMM_memory_block_list_t *get_block_list(void) {
 }
 
 void PMM_add_block(void* addr, u64 size) {
+    if(MMU_get_address_half(addr) != MMU_addr_kernel_half && MMU_make_addr_half(addr, MMU_addr_lower_half) < MMU_make_addr_half(&KERNEL_END, MMU_addr_lower_half)) {
+        u64 diff = (u64)(MMU_make_addr_half(&KERNEL_END, MMU_addr_lower_half) - MMU_make_addr_half(addr, MMU_addr_lower_half));
+        if(size < diff) return; //Block size is too small
+        size -= diff;
+        addr += diff;
+    }
+    //if(size < PMM_map_size*16) return; //Having small memory blocks is uneffective
+
     struct PMM_memory_block_list_t *block_list = get_block_list();
     if(block_list == NULL) block_list = (struct PMM_memory_block_list_t*)PMM_init();
 
@@ -70,10 +80,12 @@ void PMM_add_block(void* addr, u64 size) {
         if(MMU_get_paddr(NULL, addr) == NULL) {
             //Map 4KiB from the start of addr until the end of the bitmap. covers around 128MiB
             //What do we do if the new address is not aligned to 4KiB? Not handled as of now. TODO
-            u8 res = MMU_map_range(NULL, MMU_make_addr_half(addr, MMU_addr_lower_half), addr, bitmap_size, MMU_PAGE_4K, MMU_FLAG_RW | MMU_FLAG_SUPERVISOR | MMU_FLAG_PRESENT);
+            u8 res = MMU_map_range(NULL, MMU_make_addr_half(addr, MMU_addr_lower_half), addr, align(bitmap_size, MMU_PAGE_4K), MMU_PAGE_4K, MMU_FLAG_RW | MMU_FLAG_SUPERVISOR | MMU_FLAG_PRESENT);
             if(res != 0) return; //Cannot map address, for now.
         }
     }
+
+    for(int i = 0; (u64)((u64*)addr+i) < (u64)addr+bitmap_size+sizeof(struct PMM_memory_block_t); i++) *((u64*)addr+i) = 0;
 
     struct PMM_memory_block_t *block = (struct PMM_memory_block_t*)addr;
     block->next = block_list->addr;
@@ -81,8 +93,8 @@ void PMM_add_block(void* addr, u64 size) {
     
     block->bitmap = (u64*)((u64)addr + sizeof(struct PMM_memory_block_t));
     block->bitmap_size = bitmap_size;
-    block->addr = (u64*)MMU_make_addr_half((void*)align((u64)block->bitmap + bitmap_size, PMM_map_size), MMU_addr_lower_half);
-    block->size = size - ((u64)block->addr - (u64)block);
+    block->addr = (u64*)MMU_make_addr_half((void*)align((u64)block->bitmap + block->bitmap_size, PMM_map_size), MMU_addr_lower_half);
+    block->size = (u64)block->addr - (u64)block->bitmap - bitmap_size;
 
     block->next = NULL;
 }
@@ -97,26 +109,79 @@ void* PMM_alloc_aligned(u64 size, u64 alignment) {
     u32 page_count = size / PMM_map_size; //Number of PMM_map_size'd pages to allocate
 
     for(struct PMM_memory_block_t *block = block_list->addr; block; block = block->next) {
-        switch (page_count) {
-            case 1:
-                for(u64 *bmp = block->bitmap; (u64)bmp <= (u64)block->bitmap + block->bitmap_size; bmp++) {
-                    u64 map = ~(*bmp);
-                    if(map == 0) continue;
-                    
-                    u64 map_idx = 0;
+        if(page_count == 1) {
+            for(u64 *bmp = block->bitmap; (u64)bmp <= (u64)block->bitmap + block->bitmap_size; bmp++) {
+                u64 map = ~(*bmp);
+                if(map == 0) continue;
 
-                    asm volatile("bsr %1, %0" : "=r" (map_idx) : "r" (map) : "rax", "rbx", "flags");
+                u64 map_idx = 0;
+                asm volatile("bsr %1, %0" : "=r" (map_idx) : "r" (map) : "rax", "rbx", "flags");
 
-                    void* addr = (void*)((u64)block->addr + (((u64)bmp - (u64)block->bitmap) * 8 + (64-map_idx-1)) * PMM_map_size);
-                    if ((u64)addr >= (u64)block->addr + block->size) break;
-                    *bmp |= (u64)1 << map_idx;
+                void* addr = (void*)((u64)block->addr + (((u64)bmp - (u64)block->bitmap) * 8 + (64-map_idx-1)) * PMM_map_size);
+                if ((u64)addr+size >= (u64)block->addr + block->size) break; //If the end of the allocation goes over the end of the block, then ignore it and try again.
+                *bmp |= (u64)1 << map_idx;
 
+                return addr;
+            }
+        } else {
+            for(u64 *bmp = block->bitmap; (u64)bmp <= (u64)block->bitmap + block->bitmap_size; bmp++) {
+                u64 map = ~(*bmp);
+                if(map == 0) continue;        
+
+                u64 mask = 0;
+                u8 tmp_page_count = page_count;
+                u8 bmp_idx = 0;
+                u8 starting_map_idx = 63;
+
+                do {
+                    u64 shift_idx = 0;
+                    asm volatile("bsr %1, %0" : "=r" (shift_idx) : "r" (map) : "rax", "rbx", "flags");
+
+                    if(bmp_idx && shift_idx != 63) break; //We can only continue allocations from the MSB
+
+                    if(shift_idx+1 >= tmp_page_count) {
+                            mask = ((u64)1 << tmp_page_count) - 1;
+                            mask <<= shift_idx+1-tmp_page_count;
+                    } else {
+                        if(!bmp_idx) mask = ((u64)1 << (shift_idx+1))-1;
+                        else mask = ~(u64)0;
+                    }
+
+                    if((map & mask) == mask) {
+                        if(bmp_idx == 0) starting_map_idx = shift_idx+1;
+                        if(shift_idx+1 >= tmp_page_count) tmp_page_count = 0;
+                        else {
+                            tmp_page_count -= shift_idx+1;
+                            bmp_idx++;
+                            map = ~(*(bmp+bmp_idx));
+                        }
+                    } else {
+                        //TODO: TEST WHEN BITMAP IS FRAGMENTED
+                        //Skip MSB entries we just went over
+                        map = (map ^ mask) & map;
+                    }
+                } while(tmp_page_count && map);
+
+                if(tmp_page_count) continue;
+
+                void* addr = (void*)((u64)block->addr + (((u64)bmp - (u64)block->bitmap) * 8 + (64-starting_map_idx)) * PMM_map_size);
+                if ((u64)addr+size >= (u64)block->addr + block->size) break;
+
+                if(page_count <= starting_map_idx) {
+                    *bmp |= (((u64)1 << page_count)-1) << (starting_map_idx-page_count);
                     return addr;
                 }
-                break;
-            default: //More than one page
-                return NULL;
-                break;
+
+                *bmp |= ((u64)1 << (starting_map_idx)) - 1;
+                page_count -= starting_map_idx;
+                for(u64* bmp2 = bmp+1; bmp2 <= bmp+bmp_idx; bmp2++, page_count -= 64) {
+                    u64 newbitmap = page_count >= 64 ? ~(u64)0 : (((u64)1 << page_count)-1) << (64-page_count);
+                    *bmp2 |= newbitmap;
+                }
+
+                return addr;
+            }
+            return NULL;
         }
     }
 
