a89e5ba2d56ae4a4a1b5ceb78520c703287e0e7b
alberinfo-2nd <albertcuch@gmail.com>
2026-06-06 12:35:09 -0400

    Enable serial redirection on real hardware

--- src/kernel/debug/serial.c
+++ src/kernel/debug/serial.c
@@ -4,10 +4,11 @@
 
 #define SERIAL_COM1 0x3F8
 
-#define SERIAL_INT_ENABLE 1 //send 1 to COM1 to enable IRQs
+#define SERIAL_INT_DISABLE 0
+#define SERIAL_INT_ENABLE  1 //send 1 to COM1 to enable IRQs
 
 //LCR = Line Control Register
-#define SERIAL_LCR_DATA_SIZE    2 //This equals 7bit transmissions. This is good enough, since we will only ever send ASCII text for debugging
+#define SERIAL_LCR_DATA_SIZE    3 //8-bit transmission size.
 #define SERIAL_LCR_STOP_SHORT   0 << 2
 #define SERIAL_LCR_STOP_LONG    1 << 2
 #define SERIAL_LCR_PARITY_NONE  0 << 3
@@ -22,30 +23,41 @@
 #define SERIAL_IER_CLEAR_RECEIVE_FIFO   1 << 1
 #define SERIAL_IER_CLEAR_TRANSMIT_FIFO  1 << 2
 #define SERIAL_IER_DMA_SELECT           1 << 3
+#define SERIAL_ITL_14Bytes              0b11 << 6
 
 //MCR = Modem Control Register
-#define SERIAL_MCR_DTR      1 //Data Terminal Ready
-#define SERIAL_MCR_RTS      1 << 1 //Rquest to Send
+#define SERIAL_MCR_DTR      1 //Force data Terminal Ready
+#define SERIAL_MCR_RTS      1 << 1 //Force request to Send
+#define SERIAL_MCR_AUX1     1 << 2
+#define SERIAL_MCR_AUX2     1 << 3
+
+//LSR = Line Status Register
+#define SERIAL_LSR_EHR      (1 << 5) //Empty Transmitter Holding Register
 
 static u16 baud_rate = 1;
 
+void DEBUG_SERIAL_write(char c);
+
 //Initializes COM1
 void DEBUG_SERIAL_init() {
-    outportb(SERIAL_COM1 + 1, SERIAL_INT_ENABLE);
+    outportb(SERIAL_COM1 + 1, SERIAL_INT_DISABLE);
     
     outportb(SERIAL_COM1 + 3, SERIAL_LCR_DLAB_ENABLE);
-    outportb(SERIAL_COM1 + 1, baud_rate >> 8);
+    
     outportb(SERIAL_COM1, baud_rate & 0xFF);
+    outportb(SERIAL_COM1 + 1, baud_rate >> 8);
 
     outportb(SERIAL_COM1 + 3, SERIAL_LCR_DATA_SIZE | SERIAL_LCR_STOP_SHORT | SERIAL_LCR_PARITY_NONE);
 
-    outportb(SERIAL_COM1 + 2, SERIAL_IER_FIFO_ENABLE | SERIAL_IER_CLEAR_RECEIVE_FIFO | SERIAL_IER_CLEAR_TRANSMIT_FIFO);
+    outportb(SERIAL_COM1 + 2, SERIAL_IER_FIFO_ENABLE | SERIAL_IER_CLEAR_RECEIVE_FIFO | SERIAL_IER_CLEAR_TRANSMIT_FIFO | SERIAL_ITL_14Bytes);
+
+    outportb(SERIAL_COM1 + 4, SERIAL_MCR_DTR | SERIAL_MCR_RTS | SERIAL_MCR_AUX1 | SERIAL_MCR_AUX2);
 
     return;
 }
 
 void DEBUG_SERIAL_write(char c) {
-    while((inportb(SERIAL_COM1 + 5) & (1 << 5)) == 0); //While Transmitter Holding Register is not empty
+    while((inportb(SERIAL_COM1 + 5) & SERIAL_LSR_EHR) == 0); //While Transmitter Holding Register is not empty
 
     outportb(SERIAL_COM1, c);
 }
